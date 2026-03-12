cec27d0b33682ce87354c1d14ecc9a309823df75
Alessandro Mauro Incantalupo <alessandro.incantalupo@gmail.com>
2026-03-12 10:10:24 +0100

    fix: navbar menu works

--- apps/admin-dashboard/src/app/layouts/main-layout/components/header/navbar-menu/navbar-menu.component.ts
+++ apps/admin-dashboard/src/app/layouts/main-layout/components/header/navbar-menu/navbar-menu.component.ts
@@ -4,8 +4,6 @@ import {
   inject,
   signal,
 } from '@angular/core';
-import { Router } from '@angular/router';
-import { CustomMenuItem } from '@core/models/menu.model';
 import { MenuStore } from '@core/state/menu.store';
 import { ChipModule } from 'primeng/chip';
 import { NavbarSubmenuComponent } from './navbar-submenu/navbar-submenu.component';
@@ -19,30 +17,13 @@ import { NavbarSubmenuComponent } from './navbar-submenu/navbar-submenu.componen
 })
 export class NavbarMenuComponent {
   readonly menuStore = inject(MenuStore);
-  readonly router = inject(Router);
   readonly openIndex = signal<number | null>(null);
-
-  private showMenuClass = [
-    'scale-100',
-    'animate-fade-in-up',
-    'opacity-100',
-    'pointer-events-auto',
-  ];
-  private hideMenuClass = [
-    'scale-95',
-    'animate-fade-out-down',
-    'opacity-0',
-    'pointer-events-none',
-  ];
 
   open(i: number) {
     this.openIndex.set(i);
   }
+
   close() {
     this.openIndex.set(null);
-  }
-
-  public toggleMenu(menu: CustomMenuItem): void {
-    menu.selected = !menu.selected;
   }
 }
